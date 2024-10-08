f577d91a5a4e4655e243a0555e249eff36840a7e
rafacostavilla <rafaelcostavilla@gmail.com>
2024-10-08 13:36:39 +1300

    Try to fix the previous error using Rc smart pointer to share lock's ownership

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use std::{sync::Mutex, thread};
+use std::rc::Rc;
 
 fn main() {
-    let counter = Mutex::new(0);
+    let counter = Rc::new(Mutex::new(0));
     let mut handles = vec![];
 
     for _ in 0..10{
+        let counter = Rc::clone(&counter);
         let handle = thread::spawn(move ||{
             let mut num = counter.lock().unwrap();
             *num += 1;
